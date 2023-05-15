74700fcb67135c4a762ee098c7ae6a647b0589e8
AniTei <115176414+AniTei@users.noreply.github.com>
2023-05-15 18:53:23 +0200

    DID ALOT, DRAFTING :)

---
+++ index.js
@@ -0,0 +1,44 @@
+const contentContainer = document.querySelector (".content-container");
+
+contentContainer.innerHTML = `<p> hei </p>`;
+
+console.log ("hei");
+
+/* BOTH OK */
+
+const url = "http://thefoodplace.local/wp-json/wp/v2/posts";
+
+async function getPosts () {
+    const respond = await fetch(url);
+    const data = await respond.json();
+
+    console.log (data);
+
+    return data;
+
+/*     for (let i = 0; i < postData.length; i++) */
+
+}
+
+getPosts();
+
+console.log (data);
+
+
+/* function showData (data) {
+    console.log (data);
+
+HOW DO I RETURN A VARIABLE FROM A FUNCTION???
+
+}
+
+showData(); */
+
+
+
+
+
+
+contentContainer.innerHTML = `<div> PÅ DEG </div>`;
+
+/* DO I HAVE TO LOOP THROUGH EVERY TIME I WANT TO USE AN ARRAY? */
