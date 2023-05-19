91d65cf18cd2fb08074de7b67e0f6d3bbeb79c41
AniTei <115176414+AniTei@users.noreply.github.com>
2023-05-19 16:29:58 +0200

    styling of spec page

--- js/spec.js
+++ js/spec.js
@@ -1,18 +1,37 @@
-const specContainer = document.querySelector (".spec-container");
-    
-    
+const specContainer = document.querySelector(".spec-container");
+
+specContainer.innerHTML = `<div>test</div>`;
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const idPost = params.get("id");
+
+const url = "http://thefoodplace.local/wp-json/wp/v2/posts/" + idPost;
+
+async function findPost() {
+    try {
+        const respond = await fetch(url)
+        const data = await respond.json();
+
+        console.log(data.date);
+
+        specContainer.innerHTML += `<h1>${data.title.rendered}</h1>
+                                        <p class="date">Published: ${data.date}</p>
+
+                                        <div>${data.content.rendered}</div>`;
 
 
-specContainer.innerHTML= `<div>test</div>`   ;
-    
-    
-    /* <h1>a love letter to onions</h1>
-<p class="date">26.04.2023</p>
-<img class="spec-img" src="images/keriliwi-1ENAkCqRi80-unsplash.jpg"
-    alt="baskets with different kinds of onions">
-
-<p>Aliquam erat volutpat. </p>
- */
 
 
-    console.log ("hei");
+        detailsTitle.innerHTML = `${yourDrink.strDrink}`;
+
+    }
+    catch (error) {
+        console.log("OOOPSIE:/sjekk internettforbindelse!", error);
+        detailsContent.innerHTML = `<div class="error">OH NO, something went wrong 😢</div>`;
+    }
+}
+
+findPost();
+
+
