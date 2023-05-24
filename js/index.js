b7bc5916d2019e287cb8e1f8ede61fdaba4aa4a4
AniTei <115176414+AniTei@users.noreply.github.com>
2023-05-24 02:44:54 +0200

    think i fixed buttons, but not quite, better tried then not

--- js/index.js
+++ js/index.js
@@ -1,19 +1,12 @@
 const sliderWidth = document.querySelector(".slider-width");
 
-/* sliderWidth.innerHTML += `<p> hei </p>`; */
-
-console.log("hei");
-
-
 const url = "http://thefoodplace.local/wp-json/wp/v2/posts/?per_page=12";
 
 async function get12Posts() {
     const respond = await fetch(url);
     const data = await respond.json();
 
-    console.log(data);
-
-    for (let i=0; i< data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         sliderWidth.innerHTML += `
         <div class="item">
             <a href="specific-post.html?id=${data[i].id}">
@@ -22,32 +15,37 @@ async function get12Posts() {
 
             </a>
         </div>`;
-
     }
-
-    /*  for (let i = 0; i < data.length; i++) {
- 
-         carousel.innerHTML += `<a href="specific-post.html?id=${data[i].id}>
-         <div> <h3>${data[i].title.rendered}</h3>
-         
-         
-         </div>
-         </a>`;
-     } */
-
-
-
 }
 
 get12Posts();
 
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+const sliderContainer = document.querySelector(".slider-container");
+const slider = document.querySelector(".slider-width");
 
 
-/* const prevBtn = document.querySelector (".prev");
-const nextBtn = document.querySelector (".next");
+let count = 0;
 
-prevBtn.innerHTML = "hei";
-nextBtn.innerHTML= "sann"; */
+nextBtn.addEventListener("click", function moveSlider() {
+    count = count - slider.offsetWidth / 3;
+    if (-slider.offsetWidth < count) {
+        slider.style.left = count + "px"
+        nextBtn.disabled = false;
+    }
+})
+
+
+prevBtn.addEventListener("click", function moveSlider() {
+    count = count + slider.offsetWidth / 3;
+    if (count < 5) {
+        slider.style.left = count + "px";
+        prevBtn.disabled = false;
+    }
+})
+
 
 
 
