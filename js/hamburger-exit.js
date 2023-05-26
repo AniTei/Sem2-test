6e79fb7e5308533a2e9b8b71cd8e9dc5b089c677
AniTei <115176414+AniTei@users.noreply.github.com>
2023-05-27 00:40:52 +0200

    styled nav, also couldnt fix js menu exit

---
+++ js/hamburger-exit.js
@@ -0,0 +1,11 @@
+const main = document.querySelector ("main");
+const nav = document.querySelector ("nav");
+
+
+main.addEventListener("click", function exitMenu(){
+    nav.style.display = "none";
+    })
+
+/* main.innerHTML = "<p>hei</p>";
+
+console.log ("hallo"); */
